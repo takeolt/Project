2f24c72802576085a8696f62071749702d2657ea
takeolt <paulius476@gmail.com>
2020-02-08 21:56:03 +0100

    game version 0.1

--- StarShip/code.js
+++ StarShip/code.js
@@ -1,28 +1,56 @@
-var cvs = document.querySelector("canvas");
+//Declarations
+
+var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 var restart = document.getElementById("restart");
 
-// Variables
+//events
 
-var time = 0.001;
+window.addEventListener('resize', resizeCanvas, false);
+document.addEventListener("mousemove", mouseMove);
+
+// Variables
 
 /* --> Images */
 var ship = new Image();
 var background = new Image();
-
-ship.src = "picture/spaceship.png";
-background.src = "picture/background1.jpg";
-
-
+var bullet = new Image();
+var backGroundWidth;
+var backGroundHeigh;
+var mouse_x = 0;
+var mouse_y = 0;
 
 // functions
 
-
 function start() {
 
-    ctx.drawImage(background,0,0);
+    resizeCanvas();
+
+    ctx.drawImage(background,0,0, backGroundWidth, backGroundHeigh);
+    ctx.drawImage(ship,mouse_x,mouse_y);
+    ctx.drawImage(bullet,mouse_x + 40,mouse_y);
 
     requestAnimationFrame(start);
+}
+
+function resizeCanvas() {
+
+    backGroundWidth = window.innerWidth;
+    backGroundHeigh = window.innerHeight;
+    cvs.width = window.innerWidth;
+    cvs.height = window.innerHeight;
+
+    ship.src = "picture/spaceship.png";
+    background.src = "picture/background1.jpg";
+    bullet.src = "picture/bullet.jpg";
+}
+
+function mouseMove(events) {
+    var tempx = event.clientX;
+    var tempy = event.clientY;
+
+    mouse_x = tempx;
+    mouse_y = tempy;
 }
 
 
