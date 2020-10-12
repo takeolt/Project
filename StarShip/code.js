5da8df33ea5122f3e9db753d9cd1f18e90669f36
Paulius Deveika <paulius476@gmail.com>
2020-10-12 20:29:50 +0200

    New version

--- StarShip/code.js
+++ StarShip/code.js
@@ -15,9 +15,9 @@ document.addEventListener("click", mouseClickEffect)
 /* --> Images */
 var ship = new Image();
 var background = new Image();
-var bullet = new Image();
-var backGroundWidth;
-var backGroundHeigh;
+var simple_bullet = new Image();
+var back_Ground_Width;
+var back_Ground_Heigh;
 var mouse_x = 0;
 var mouse_y = 0;
 var test_p = new Image();
@@ -30,11 +30,12 @@ var enemy_array = [];
 
 // booleans
 
-var gameStart = true;
-var shootAnimationOn = false;
-var phase2 = false;
-var phase1 = true;
-var spawnEnemy = true;
+var game_Start = true;
+var shoot_AnimationOn = false;
+var level2 = false;
+var level1 = true;
+var spawn_Enemy = true;
+var timer_out_draw_enemy = true;
 
 // functions
 
@@ -42,20 +43,38 @@ function start() {
 
     resizeCanvas();
 
-    ctx.drawImage(background, 0, 0, backGroundWidth, backGroundHeigh);
+    if(mouse_x + ship.width >= cvs.width) {
+        mouse_x = cvs.width - ship.width;
+    }
+    if(mouse_y + ship.height >= cvs.height) {
+        mouse_y = cvs.height - ship.height;
+    }
+
+    ctx.drawImage(background, 0, 0, back_Ground_Width, back_Ground_Heigh);
     ctx.drawImage(ship, mouse_x, mouse_y);
     //ctx.drawImage(test_p, mouse_x, mouse_y);
+
+    checkForBulletContact();
     
+    if(timer_out_draw_enemy && level1) {
+        SpawnEnemies();
+        timer_out_draw_enemy = false;
+        setTimeout(function(){timer_out_draw_enemy = true}, 1000);
+    }
 
-   // if(enemy_array.length > 0) {
-     //   drawEnemies();
-    //}
+    if(timer_out_draw_enemy && level2) {
+        
+    }
 
-    if(shootAnimationOn) {
+   if(enemy_array.length > 0) {
+        drawEnemies();
+    }
+
+    if(shoot_AnimationOn) {
         drawBullets();
 
         if(bullets.length == 0) {
-            shootAnimationOn = false;
+            shoot_AnimationOn = false;
         }
     }
 
@@ -64,14 +83,14 @@ function start() {
 
 function resizeCanvas() {
 
-    backGroundWidth = window.innerWidth;
-    backGroundHeigh = window.innerHeight;
+    back_Ground_Width = window.innerWidth;
+    back_Ground_Heigh = window.innerHeight;
     cvs.width = window.innerWidth;
     cvs.height = window.innerHeight;
 
     ship.src = "picture/spaceship_model_1.png";
     background.src = "picture/background1.jpg";
-    bullet.src = "picture/bullet_model_1.jpg";
+    simple_bullet.src = "picture/bullet_model_1.jpg";
     test_p.src = "picture/temp.jpg";
     monster_type_1.src = "picture/monster_type_1.png";
 }
@@ -82,7 +101,6 @@ function mouseMoveEffect(events) {
 
     mouse_x = tempx;
     mouse_y = tempy;
-
     
 }
 
@@ -91,43 +109,51 @@ function mouseClickEffect(events) {
     var temp = bullets.length;
 
     bullets[temp] = {
+        type : simple_bullet,
         id : temp,
         x : mouse_x + 30,
-        y1 : mouse_y + 7.5,
-        y2 : mouse_y + 28
+        y : mouse_y + 7.5
     }
 
-    shootAnimationOn = true;
+    temp++;
+
+    bullets[temp] = {
+        type : simple_bullet,
+        id : temp,
+        x : mouse_x + 30,
+        y : mouse_y + 28
+    }
+    shoot_AnimationOn = true;
 }
 
 
 function drawBullets() {
 
     for(i = 0; i < bullets.length; i++) {
-
-        ctx.drawImage(bullet, bullets[i].x, bullets[i].y1);
-        ctx.drawImage(bullet, bullets[i].x, bullets[i].y2);
-
-        bullets[i].x += 7;
+        
+      
+        ctx.drawImage(simple_bullet, bullets[i].x, bullets[i].y);
+        
+        bullets[i].x += 5;
 
     }
 
     if(bullets[0].x >= cvs.width) {
-       bullets.shift();
+       bullets.shift(0,1);
     } 
 
-    console.log(bullets.length);
+
 }
 
 
 function SpawnEnemies() {
 
-    if(phase1) {
+    if(level1) {
         var tempY = Math.floor(Math.random() * cvs.height);
         var size = enemy_array.length;
 
         if(tempY + monster_type_1.height > cvs.height) {
-            tempY -= monster_type_1;
+            tempY -= monster_type_1.height;
         }
 
         if(tempY < monster_type_1.height ) {
@@ -135,26 +161,48 @@ function SpawnEnemies() {
         }
 
         enemy_array[size] = {
-            enemy_type: "mob 1",
-            enemy : monster_type_1,
+            level: "level_1",
+            type : monster_type_1,
             x : cvs.width,
             y : tempY
         }
     }
     
 
-    if(gameStart) {
-        gameStart = false;
-        setTimeout(function(){phase1 = false; phase2 = true;}, (10*1000));
+    if(game_Start) {
+        game_Start = false;
+        setTimeout(function(){level1 = false; level2 = true;}, (100*1000));
     }
 }
 
 function drawEnemies() {
 
-    if(phase1) {
+    if(enemy_array[0].level == "level_1") {
+
         for(i = 0; i < enemy_array.length; i++) {
-            ctx.drawImage(enemy_array[i].enemy, enemy_array[i].x, enemy_array[i].y);
+
+            if(enemy_array[i].level.localeCompare("level_1") != 0) {   
+                break;
+            }
+
+            ctx.drawImage(enemy_array[i].type, enemy_array[i].x, enemy_array[i].y);
             enemy_array[i].x -= 3;
+        }
+    }
+
+    if(enemy_array[0].x == 0){
+        enemy_array.slice(1,0);
+    } 
+}
+
+function checkForBulletContact() {
+    for(i = 0; i < bullets.length; i++) {
+        for(a = 0; a < enemy_array.length; a++) {
+            if(bullets[i].x + bullets[i].type.width >= enemy_array[a].x && bullets[i].x <= enemy_array[a].x + enemy_array[a].type.width
+                && bullets[i].y - bullets[i].type.height <= enemy_array[a].y && bullets[i].y + 1 >= enemy_array[a].y - enemy_array[a].type.height) {
+                    enemy_array.slice(1,a);
+                    console.log("Hit");
+            }
         }
     }
 }
